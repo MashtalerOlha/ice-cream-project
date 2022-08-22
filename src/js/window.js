57c60e4df6b7863784be8a5902617d9ec2444f06
MashtalerOlha <87094132+MashtalerOlha@users.noreply.github.com>
2022-08-22 20:30:54 +0300

    init

--- src/js/window.js
+++ src/js/window.js
@@ -3,10 +3,12 @@
     openModalBtn: document.querySelector('[data-modal-open-buy]'),
     closeModalBtn: document.querySelector('[data-modal-close-buy]'),
     modal: document.querySelector('[data-modal-buy'),
+    mobmod: document.querySelector('[data-modal-open-mod]')
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.mobmod.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
